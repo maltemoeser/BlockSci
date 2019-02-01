6a0e0f903b47ef267fb28d3f39f3ee2908195def
Malte Möser <ilovegit@maltemoeser.de>
2019-02-01 12:56:48 -0500

    Use mtime_cache and cache pip

--- .travis/compile_osx_cpp.sh
+++ .travis/compile_osx_cpp.sh
@@ -1,7 +1,9 @@
+cd "$TRAVIS_BUILD_DIR"
 mkdir -p release
 cd release
 cmake -DCMAKE_BUILD_TYPE=Release -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DCMAKE_INSTALL_PREFIX=$HOME/.local ..
 mtime_cache ../**/*.{%{cpp}} -c ../.mtime_cache/cache.json
 make -j 2
 make install
-cd ..
+mtime_cache "$HOME"/.local/**/*.{%{cpp}} -c "$TRAVIS_BUILD_DIR"/.mtime_cache/blocksci.json
+cd "$TRAVIS_BUILD_DIR"
