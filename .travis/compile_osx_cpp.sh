61f837f1e7eef4c5df486ad25574d51846f937dc
Malte Möser <ilovegit@maltemoeser.de>
2019-02-01 10:37:26 -0500

    Make install without sudo

--- .travis/compile_osx_cpp.sh
+++ .travis/compile_osx_cpp.sh
@@ -3,5 +3,5 @@ cd release
 cmake -DCMAKE_BUILD_TYPE=Release -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl ..
 mtime_cache ../**/*.{%{cpp}} -c ../.mtime_cache/cache.json
 make -j 2
-sudo make install
+make install
 cd ..
--- .travis/compile_ubuntu_cpp.sh
+++ .travis/compile_ubuntu_cpp.sh
@@ -3,5 +3,5 @@ cd release
 CC=gcc-7 CXX=g++-7 cmake -DCMAKE_BUILD_TYPE=Release ..
 mtime_cache ../**/*.{%{cpp}} -c ../.mtime_cache/cache.json
 make -j 2
-sudo make install
+make install
 cd ..
