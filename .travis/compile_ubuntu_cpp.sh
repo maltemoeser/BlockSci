3ea60bf1b479c6e22e36da22cfba5b5bc9b6c5fd
Malte Möser <ilovegit@maltemoeser.de>
2019-02-01 13:37:44 -0500

    Add mtime to cpp and make config os-dependent

--- .travis/compile_ubuntu_cpp.sh
+++ .travis/compile_ubuntu_cpp.sh
@@ -5,4 +5,5 @@ CC=gcc-7 CXX=g++-7 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$HOME
 mtime_cache ../**/*.{%{cpp}} -c ../.mtime_cache/cache.json
 make -j 2
 make install
+mtime_cache "$HOME"/.local/**/*.{%{cpp}} -c "$TRAVIS_BUILD_DIR"/.mtime_cache/blocksci.json
 cd "$TRAVIS_BUILD_DIR"
